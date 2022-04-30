0945bcd7db5941de8fbcfb8bba9b1fc0f1f94f7d
irboi746 <88809233+irboi746@users.noreply.github.com>
2022-04-30 16:50:01 +0800

    Update recursive_hash.bat

--- Host/recursive_hash.bat
+++ Host/recursive_hash.bat
@@ -1,11 +1,15 @@
 @echo off
-for /R "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01" %%f in (*) do ( 
-   echo %%f >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
-   echo MD5 >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
-   certutil -hashfile C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\sampleA1_.exe md5 | findstr /R "^[a-fA-F0-9]*$" >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
-   echo sha1 >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
-   certutil -hashfile C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\sampleA1_.exe sha1 | findstr /R "^[a-fA-F0-9]*$" >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
-   echo sha256 >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
-   certutil -hashfile C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\sampleA1_.exe sha256 | findstr /R "^[a-fA-F0-9]*$" >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
-   echo: >> "C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\output.txt"
+
+for /R %1 %%f in (*) do ( 
+   echo %%f >> "%1\output.txt"
+   echo MD5 >> "%1\output.txt"
+   certutil -hashfile C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\sampleA1_.exe md5 | findstr /R "^[a-fA-F0-9]*$" >> "%1\output.txt"
+
+   echo sha1 >> "%1\output.txt"
+   certutil -hashfile C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\sampleA1_.exe sha1 | findstr /R "^[a-fA-F0-9]*$" >> "%1\output.txt"
+
+   echo sha256 >> "%1\output.txt"
+   certutil -hashfile C:\Users\AdminELS\Desktop\Lab_02_Samples\SET_01\sampleA1_.exe sha256 | findstr /R "^[a-fA-F0-9]*$" >> "%1\output.txt"
+
+   echo: >> "%1\output.txt"
 )
